6e7528745c67889e19d8b0446ddee2293268659c
Kevin Unger <kevinunger98@gmail.com>
2019-04-05 14:36:30 +0200

    Added Game

---
+++ BlockRunnerGame/Assets/Scenes/Scripts/Credits.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public void Quit()
+    {
+
+        Debug.Log("Quit");
+        Application.Quit();
+
+    }
+    
+        
+    
+}
---
+++ BlockRunnerGame/Assets/Scenes/Scripts/gameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject WinPanelUI;
+
+
+    public void EndGame ()
+    {
+
+
+        if (gameHasEnded == false)
+        {
+
+            gameHasEnded = true;
+            Debug.Log("Game over");
+            Invoke("Restart", restartDelay);
+
+
+        }
+
+
+    }
+
+
+
+
+    public void CompleteLevel()
+    {
+
+        WinPanelUI.SetActive(true);
+        Debug.Log("1");
+
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+
+
+
+
+    }
+
+
+
+}
